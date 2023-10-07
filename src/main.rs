d62e0605a0cd2a6ee020fdd8833083bc78d0409c
Hieu Nguyen <macbookM1@MacBook-Pro.local>
2023-10-07 20:16:20 +0700

    add health_check endpoint and test

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("Worlddddd");
-    format!("Hello {}!", &name)
-}
+use std::net::TcpListener;
+
+use actix_api::run;
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
+    run(listener)?.await
 }
