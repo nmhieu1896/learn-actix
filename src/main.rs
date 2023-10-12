24019ce76647a35f61b39f71a5de856681f64e59
Hieu Nguyen <macbookM1@MacBook-Pro.local>
2023-10-12 15:30:30 +0700

    update test

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ use std::net::TcpListener;
 
 use actix_api::configuration::get_configuration;
 use actix_api::startup::run;
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address).expect("Failed to bind port 8000");
-    run(listener)?.await
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
